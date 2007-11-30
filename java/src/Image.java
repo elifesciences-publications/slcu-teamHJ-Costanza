1f0c513e8a641bba264c242d92cc1f10d7a151eb
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-11-30 23:48:00 +0000

    Initial change to Image class

--- java/src/Image.java
+++ java/src/Image.java
@@ -1,8 +1,35 @@
-public class Image {
+import java.lang.Number;
+import java.util.Vector;
 
-	private java.util.Vector intensity;
+/** Container class for pixel information */
+public class Image
+{
+    /** Greyscale channel */
+    private Vector<Float> grey;
 
-	public int height;
+    /** Width of image */
+    private int width;
 
-	public int width;
+    /** Height of image */
+    private int height;
+
+    /** Sets the width of the image. */
+    public void setWidth(int width) {
+	this.width = width;
+    }
+
+    /** Returns the width of the image. */
+    public int getWidth() {
+	return width;
+    }
+
+    /** Sets the height of the image. */
+    public void setHeight(int height) {
+	this.height = height;
+    }
+
+    /** Returns the width of the image. */
+    public int getHeight() {
+	return height;
+    }
 }
