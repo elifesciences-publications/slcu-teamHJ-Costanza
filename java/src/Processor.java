2cc8b49cfff12ea5e6125918e59589fabcde018f
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-11-30 23:20:54 +0000

    Started writing processor.

--- java/src/Processor.java
+++ java/src/Processor.java
@@ -1,10 +1,27 @@
+/**
+ * Base class for a Processor.
+ * @see Inverter
+ */
 public abstract class Processor {
 
 	public Case myCase;
 	public Processor myProcessor;
 	public Options myOptions;
 
+	/**
+	 * Get the options used by this Processor.
+	 * @return the Options object this Processor uses.
+	 */
 	public Options getOptions() {
-		return null;
+		return myOptions;
+	}
+
+	/** 
+	 * Abstract method for running the implemented process.
+	 * @param case the Case the the Processor will work on.
+	 * @return the processed Case.
+	 */
+	public abstract Case process(Case case){
+		return case;
 	}
 }
