9dbf8af378ee7258a448cf70df70fd7fdd04c14b
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-12-01 23:30:31 +0000

    Plugin can now invert.

--- plugin/src/Costanza_Plugin.java
+++ plugin/src/Costanza_Plugin.java
@@ -2,6 +2,7 @@ import Costanza.Case;
 import Costanza.Driver;
 import Costanza.Factory;
 import Costanza.Image;
+import Costanza.Inverter;
 import Costanza.Job;
 import Costanza.Options;
 import Costanza.Queue;
@@ -10,7 +11,10 @@ import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.plugin.filter.PlugInFilter;
+import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Costanza_Plugin implements PlugInFilter {
     private Factory factory;
@@ -23,29 +27,25 @@ public class Costanza_Plugin implements PlugInFilter {
     }
     
     public void run(ImageProcessor imageProcessor) {
-        imageProcessor.convertToFloat();
         ImageStack imageStack = imagePlus.getStack();
         int slices = imageStack.getSize();
-        
-        IJ.showMessage("Costanza", "Stack");
         
         Stack stack = new Stack();
         
         for (int n = 1; n <= slices; ++n) {
             ImageProcessor sliceProcessor = imageStack.getProcessor(n);
-            int width = imageProcessor.getWidth();
-            int height = imageProcessor.getHeight();
-            
-            IJ.showMessage("Costanza", "width = " + width + " : height = " + height + "");
+            ImageProcessor floatProcessor = sliceProcessor.convertToFloat();
+            int width = floatProcessor.getWidth();
+            int height = floatProcessor.getHeight();
             
             Image image = new Image(width, height);
             for (int x = 0; x < width; ++x) {
                 for (int y = 0; y < height; ++y) {
-                    float value = sliceProcessor.getf(x, y);
-                    java.lang.System.err.println(x + " " + y + " " + value);
+                    float value = floatProcessor.getf(x, y) / 255.0f;
                     image.setIntensity(x, y, value);
                 }
             }
+            
             try {
                 stack.addImage(image);
             } catch (Exception exception) {
@@ -54,10 +54,18 @@ public class Costanza_Plugin implements PlugInFilter {
         }
         
         
-        IJ.showMessage("Costanza", "Queue");
-        
-        IJ.showMessage("Costanza", "Slices = " + stack.getDepth() + "");
         Case IJCase = new Case(stack);
+        
+        IJ.showMessage("Costanza", "Step one finished!");
+        
+        Inverter inverter = new Inverter();
+        try {
+            inverter.process(IJCase, new Options());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        IJ.showMessage("Costanza", "Step two finished!");
         
 //        Queue jobs = new Queue();
 //        try {
@@ -66,12 +74,30 @@ public class Costanza_Plugin implements PlugInFilter {
 //            exception.printStackTrace();
 //        }
         
-        IJ.showMessage("Costanza", "Predriver");
-        
 //        Driver driver = new Driver(jobs, IJCase, factory);
+//        driver.run();
         
-        //       driver.run();
-        IJ.showMessage("Costanza", "Finished!");
+        
+        Stack result = IJCase.getStack();
+        int width = result.getWidth();
+        int height = result.getHeight();
+        
+        ImageStack is = new ImageStack(width, height);
+        
+        for (int i = 0; i < result.getDepth(); ++i) {
+            Image image = result.getImage(i);
+            FloatProcessor fp = new FloatProcessor(image.getWidth(), image.getHeight());
+            
+            for (int x = 0; x < width; ++x) {
+                for (int y = 0; y < height; ++y) {
+                    float value = image.getIntensity(x, y) * 255.0f;
+                    fp.setf(x, y, value);
+                }
+            }
+            is.addSlice("test", fp);
+        }
+        ImagePlus ip = new ImagePlus("Test Image", is);
+        ip.show();
     }
     
     private void initFactory() {
