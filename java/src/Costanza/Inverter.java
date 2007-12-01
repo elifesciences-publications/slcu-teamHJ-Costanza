6d487eb2cc596a4091cb4ba1c0bd92cbf8714d0b
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-12-01 12:47:07 +0000

    Removed comments.

--- java/src/Costanza/Inverter.java
+++ java/src/Costanza/Inverter.java
@@ -9,20 +9,14 @@ package Costanza;
 public class Inverter extends Processor {
     
     public Case process(Case c, Options options) throws Exception {
-        System.out.println("Inside process");
         Stack stack = c.getStack();
         if(stack == null){
             throw new Exception("No working stack initialised in case");
         }
-        System.out.println("Inside process 1");
         float max = stack.getMaxIntensityLimit();
-        System.out.println("Inside process 2");
         int depth = stack.getDepth();
-        System.out.println("Inside process3");
         int height = stack.getHeight();
-        System.out.println("Inside process4");
         int width = stack.getWidth();
-        System.out.println("Inside process5");
         
         for (int zI=0; zI<depth; ++zI) {
             for (int yI=0; yI<height; ++yI) {
