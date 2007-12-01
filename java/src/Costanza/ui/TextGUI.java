3ed6c33c04dd23993748bd4377bdfd56dd2610aa
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-12-01 02:14:33 +0000

    New Build stuff.

--- java/src/Costanza/ui/TextGUI.java
+++ java/src/Costanza/ui/TextGUI.java
@@ -1,4 +1,10 @@
 package Costanza.ui;
+
+import Costanza.Image;
+import Costanza.Stack;
+import Costanza.Inverter;
+import Costanza.Case;
+
 /*
  * TextGUI.java
  *
@@ -16,6 +22,19 @@ public class TextGUI {
     
     /** Creates a new instance of TextGUI */
     public TextGUI() {
+        Image image = new Image(10, 10);
+        for(int i=0; i<image.getWidth(); ++i){
+            for(int j=0; j<image.getHeight(); ++j){
+                image.setIntensity(i, j, (i == j) ? 1 : 0);
+                System.out.print("");
+            }
+            System.out.println();
+        }
+        Stack stack = new Stack();
+        stack.addImage(image);
+        Case myCase = new Case(stack);
+        Inverter inverter = new Inverter();
+        myCase = inverter.process(myCase);
     }
     
     public static void main(String[] argv){
