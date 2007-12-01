c9e165daf5f360276d4bd8598ee328487843b522
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-12-01 07:29:52 +0000

    Added the Process MeanFilter. Not yet ready.

---
+++ java/src/Costanza/MeanFilter.java
@@ -0,0 +1,23 @@
+package Costanza;
+
+/**
+ * A meqn intesnsity filter for smothening the stack.
+ * 
+ * This is a naive implementation, that just restarts calculating the
+ * filter contribution from scratch. It uses a radial parameter to
+ * define the sphere from which the men is calcultated.
+ *
+ * @para It uses a radius 
+ * @see Processor
+ */
+public class MeanFilter extends Processor {
+
+
+    public Case process(Case c) {
+
+
+
+
+        return c;
+    }
+}
