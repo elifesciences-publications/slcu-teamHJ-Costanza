df739214538f50493679b394b369eae81ef69799
Pawel Krupinski <pawel@thep.lu.se>
2007-12-01 13:01:04 +0000

    Pixel class to make writing data for processors easier

---
+++ java/src/Costanza/Pixel.java
@@ -0,0 +1,62 @@
+package Costanza;
+
+/**Pixel is tripple of indices. It's used by Data classes
+ */
+public class Pixel {
+    
+    /** Pixel indices in x, y, z directions*/
+    private int x, y, z;
+    
+    /**Create uninitialized Pixel*/
+    public Pixel(){}
+    
+    /**Create Pixel with indices x, y and z*/
+    public Pixel( int x,  int y, int z )
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    /**Get Pixel index in x direction.
+     *@return int
+     **/
+    public int getX(){
+        return x;
+    }
+    
+    /**Gets Pixel index in y direction.
+     *@return int
+     **/
+    public int getY(){
+        return y;
+    }
+    
+    /**Gets Pixel index in z direction.
+     *@return int
+     **/
+    public int getZ(){
+        return z;
+    }
+    
+    /**Gets Pixel index in x direction.
+     *@param int
+     **/
+    public void setX( int x ){
+        this.x = x;
+    }
+    
+    /**Sets Pixel index in y direction.
+     *@param int
+     **/
+    public void setY( int y ){
+        this.y = y;
+    }
+    
+    /**Get Pixel index in z direction.
+     *@param int
+     **/
+    public void setZ( int z ){
+        this.z = z;
+    }
+}
