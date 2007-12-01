bb5ff1a7120aaa1b328d2309b209e0a7b6cfe562
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-12-01 00:30:14 +0000

    Implemented the Processor Inverter.

--- java/src/Costanza/Inverter.java
+++ java/src/Costanza/Inverter.java
@@ -1,7 +1,24 @@
+/**
+ * Invert a stack by setting all intensity values to
+ * stack.maxIntensityLimit()-stack.Intensity(xI,yI,zI).
+ *
+ * @see         Processor
+ */
 package Costanza;
 public class Inverter extends Processor {
-    
+		
     public Case process(Case c) {
-        return c;
+				
+				float max=c.getStack().getMaxIntensityLimit();
+				
+				for (int zI=0; zI<c.getStack().getDepth(); ++zI) {
+						for (int yI=0; yI<c.getStack().getHeight(); ++yI) {
+								for (int xI=0; xI<c.getStack().getWidth(); ++xI) {
+										c.getStack().setIntensity(xI,yI,zI,max-
+																							c.getStack().getIntensity(xI,yI,zI));
+								}
+						}
+				}
+				return c;
     }
 }
