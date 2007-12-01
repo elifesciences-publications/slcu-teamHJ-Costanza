b8300970da3ec43478302af445574ede1434078b
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-12-01 11:54:49 +0000

    Started working on the PeakMerger

---
+++ java/src/Costanza/PeakMerger.java
@@ -0,0 +1,30 @@
+/*
+ * PeakMerger.java
+ *
+ * Created on December 1, 2007, 12:49 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package Costanza;
+
+/**
+ *
+ * @author pontus
+ */
+public class PeakMerger extends Processor{
+    
+    /** Creates a new instance of PeakMerger */
+    public PeakMerger() {
+    }
+ /** 
+  * Implements the PeakMerger algorithm.
+  * @param a Case and an Option
+  * @returns a modified Case object
+  * @see Processor
+  */   
+ public Case process(Case c, Options o) throws Exception {
+        return c;
+    }
+}
