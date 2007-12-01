d851140f14d9ab8eb929fe4ebb167fb0b528d13f
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-12-01 01:03:25 +0000

    Added a simple TextGui.

---
+++ java/src/TextGUI.java
@@ -0,0 +1,23 @@
+/*
+ * TextGUI.java
+ *
+ * Created on December 1, 2007, 12:50 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ * The Text version of our GUI.
+ * @author michael
+ */
+public class TextGUI {
+    
+    /** Creates a new instance of TextGUI */
+    public TextGUI() {
+    }
+    
+    public static void main(String[] argv){
+        new TextGUI();
+    }
+}
