e7e11d946c260dbd5888a63fe8944cb974f89bc1
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-12-01 19:32:46 +0000

    added skeleton for PeakRempver

---
+++ java/src/Costanza/PeakRemover.java
@@ -0,0 +1,31 @@
+/*
+ * PeakRemover.java
+ *
+ * Created on December 1, 2007, 8:24 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package Costanza;
+
+/**
+ *
+ * @author pontus
+ */
+public class PeakRemover extends Processor {
+    
+    /** Creates a new instance of PeakRemover */
+    public PeakRemover() {
+    }
+    
+    /**
+     * Implements the PeakRemover algorithm.
+     * @param a Case and an Option
+     * @returns a modified Case object
+     * @see Processor
+     */
+    public Case process(Case c, Options o) throws Exception {
+        return c;
+    }
+}
