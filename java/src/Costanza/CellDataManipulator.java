9023e96513a55193e38b54b420680b1419190d7b
Pawel Krupinski <pawel@thep.lu.se>
2007-12-02 00:36:50 +0000

    CellDataManipulator.class merger implemented

--- java/src/Costanza/CellDataManipulator.java
+++ java/src/Costanza/CellDataManipulator.java
@@ -26,59 +26,61 @@ public class CellDataManipulator {
         switch(id){
             case cellCenters:{
                 
-                CellCenter c1 = null, c2 = null;
+                CellCenter e1 = null, e2 = null;
                 int breaker = 0;
-                while(iter.hasNext() || breaker < 2){
-                    CellCenter center = (CellCenter)iter.next();
-                    if(center.getId() == c1Id){
-                        c1 = center;
+                while(iter.hasNext() && breaker < 2){
+                    CellCenter elem = (CellCenter)iter.next();
+                    if(elem.getId() == c1Id){
+                        e1 = elem;
                         ++breaker;
-                    } else if(center.getId() == c2Id){
-                        c2 = center;
+                    } else if(elem.getId() == c2Id){
+                        e2 = elem;
                         ++breaker;
                     }
                 }
-                if( c1 == null || c2 == null ){
-                    System.out.println("Cell id: " + c1.getId() + " or " + c2.getId() + " not found in cellCenters Data.");
+                if( e1 == null || e2 == null ){
+                    System.out.println("Cell id: " + e1.getId() + " or " + e2.getId() + " not found in the Data.");
                     return;
                 }
                 
-                int x = (c2.getX() + c1.getX())/2;
-                int y = (c2.getY() + c1.getY())/2;
-                int z = (c2.getZ() + c1.getZ())/2;
+                int x = (e2.getX() + e1.getX())/2;
+                int y = (e2.getY() + e1.getY())/2;
+                int z = (e2.getZ() + e1.getZ())/2;
                 
                 
-                CellCenter midC = new CellCenter(data.newCellId(), x, y, z);
+                CellCenter midC = new CellCenter(newCellId, x, y, z);
                 
-                data.removeData(DataId.cellCenters, c1);
-                data.removeData(DataId.cellCenters, c2);
+                data.removeData(DataId.cellCenters, e1);
+                data.removeData(DataId.cellCenters, e2);
                 
                 data.attachData(DataId.cellCenters, midC);
                 
             }
             case cellBasinsOfAtraction:{
-                BOA b1 = null, b2 = null;
+                BOA e1 = null, e2 = null;
                 int breaker = 0;
-                while(iter.hasNext() || breaker < 2){
-                    BOA basin = (BOA)iter.next();
-                    if(basin.getId() == c1Id){
-                        b1 = basin;
+                while(iter.hasNext() && breaker < 2){
+                    BOA elem = (BOA)iter.next();
+                    if(elem.getId() == c1Id){
+                        e1 = elem;
                         ++breaker;
-                    } else if(basin.getId() == c2Id){
-                        b2 = basin;
+                    } else if(elem.getId() == c2Id){
+                        e2 = elem;
                         ++breaker;
                     }
                 }
-                if( b1 == null || b2 == null ){
-                    System.out.println("Cell id: " + b1.getId() + " or " + b2.getId() + " not found in BOA Data.");
+                if( e1 == null || e2 == null ){
+                    System.out.println("Cell id: " + e1.getId() + " or " + e2.getId() + " not found in the Data.");
                     return;
                 }
                 
-                BOA mergedB = new BOA( data.newCellId(), b1.getPixels() );
-                mergedB.addPixels(b2.getPixels());
+                BOA mergedB = new BOA( newCellId, e1.getPixels() );
+                mergedB.addPixels(e2.getPixels());
                 
-                data.removeData(DataId.cellBasinsOfAtraction, b1);
-                data.removeData(DataId.cellBasinsOfAtraction, b2);
+                data.removeData(DataId.cellBasinsOfAtraction, e1);
+                data.removeData(DataId.cellBasinsOfAtraction, e2);
+                
+                data.attachData(DataId.cellBasinsOfAtraction, mergedB);
             }
         }
         
