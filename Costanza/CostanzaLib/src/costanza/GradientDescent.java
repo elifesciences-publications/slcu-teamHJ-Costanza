827bcd03559c4c6fbfd3b160784ebbd0cdb4b8a1
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-12-06 21:30:09 +0000

    Removed some bugchecking printouts from GradientDescent. Fixes #3.

--- Costanza/CostanzaLib/src/costanza/GradientDescent.java
+++ Costanza/CostanzaLib/src/costanza/GradientDescent.java
@@ -19,17 +19,17 @@ import java.util.Vector;
  * @see Processor
  */
 public class GradientDescent extends Processor {
-    
-    /**
-     * Implementation of the Gradient descent algorithm.
-     *  @param a Case to work on.
-     *  @return a modified Case.
-		 * @todo Recursively add plateau pixels.
-		 * @todo Expand the background appropriately.
-     */
-    public Case process(Case c, Options o) throws Exception {
-        
-        System.out.println("GradientDescent::process");
+	
+	/**
+	 * Implementation of the Gradient descent algorithm.
+	 * @param c a Case to work on.
+	 * @return a modified Case.
+	 * @todo Recursively add plateau pixels.
+	 * @todo Expand the background appropriately.
+	 */
+	public Case process(Case c, Options o) throws Exception {
+		
+			//System.out.println("GradientDescent::process");
         if( c.getStack()==null) {
             throw new Exception("No working stack initialised in case from gradientdescent");
         }
@@ -42,23 +42,18 @@ public class GradientDescent extends Processor {
         // Marker for which pixels that have been visited
         int [][][] flag = new int [width][height][depth];
         for (int x=0; x<width; ++x ) {
-            for (int y=0; y<height; ++y ) {
-                for (int z=0; z<depth; ++z ) {
-                    flag[x][y][z]=0;
-                }
-            }
+					for (int y=0; y<height; ++y ) {
+						for (int z=0; z<depth; ++z ) {
+							flag[x][y][z]=0;
+						}
+					}
         }
-				System.out.println("Try to extract the background.");
         //Get background and set flag for background pixels to -1
         StackBackground sb = (StackBackground)c.getStackData(DataId.stackBackground);
         Vector<Pixel> bg = null;
         if (sb!=null) {
-					System.out.println("Size of background: " + c.sizeOfData(DataId.stackBackground)); 
-					System.out.println("Test1.");
-					System.out.println("Test2.");
 					bg = new Vector<Pixel>(sb);
 					int bgSize = bg.size();
-					System.out.println("Size of background: " + bg.size()); 
 					for (int i=0; i<bgSize; ++i ) {
 						flag[ bg.elementAt(i).getX() ][ bg.elementAt(i).getY() ]
 							[ bg.elementAt(i).getZ() ]=-1;
@@ -67,45 +62,45 @@ public class GradientDescent extends Processor {
 					bg = new Vector<Pixel>();
 					bg.setSize(0);
         }
-				System.out.println("Happily extracted the background.");
+				//System.out.println("GradDesc: Happily extracted " + bg.size() + " background pixels.");
         int count=1;
         //Find the maxima from each pixel
         for (int zStart=0; zStart<depth; ++zStart) {
-            for (int yStart=0; yStart<height; ++yStart) {
-                for (int xStart=0; xStart<width; ++xStart) {
-                    int x = xStart;
-                    int y = yStart;
-                    int z = zStart;
-                    Vector<Pixel> walkTmp = new Vector<Pixel>();//positions for one walk (start point)
-                    walkTmp.add(new Pixel(x,y,z));
-                    
-                    if (x==0 && y==0) {
-                        System.out.println("Stack: " + z);
-                    }
-                    //find the max by walking uphill (greedy)
-                    double value,newValue;
-                    if (flag[x][y][z]==0) {
-                        do {
-                            newValue=value=c.getStack().getIntensity(x,y,z);
-                            int xNew=x, yNew=y, zNew=z;
-                            
-                            //Check all pixels around a given pixel
-                            for (int zz=z-1; zz<=z+1; ++zz) {
-                            		for (int yy=y-1; yy<=y+1; ++yy) {
-                            				for (int xx=x-1; xx<=x+1; ++xx) {
-                            						if (xx>=0 && yy>=0 && zz>=0 &&
-                            								xx<width && yy<height && zz<depth)
-                            								if (c.getStack().getIntensity(xx,yy,zz)>newValue) {
-                            										newValue=c.getStack().getIntensity(xx,yy,zz);
-                            										xNew = xx;
-                            										yNew = yy;
-                            										zNew = zz;
-                            								}
-                            				}
-                            		}
-                            }
-                            
-                            //Check nearest neighbors
+					for (int yStart=0; yStart<height; ++yStart) {
+						for (int xStart=0; xStart<width; ++xStart) {
+							int x = xStart;
+							int y = yStart;
+							int z = zStart;
+							Vector<Pixel> walkTmp = new Vector<Pixel>();//positions for one walk (start point)
+							walkTmp.add(new Pixel(x,y,z));
+              
+							//if (x==0 && y==0) {
+							//System.out.println("Stack: " + z);
+							//}
+							//find the max by walking uphill (greedy)
+							double value,newValue;
+							if (flag[x][y][z]==0) {
+								do {
+									newValue=value=c.getStack().getIntensity(x,y,z);
+									int xNew=x, yNew=y, zNew=z;
+                  
+									//Check all pixels around a given pixel
+									for (int zz=z-1; zz<=z+1; ++zz) {
+										for (int yy=y-1; yy<=y+1; ++yy) {
+											for (int xx=x-1; xx<=x+1; ++xx) {
+												if (xx>=0 && yy>=0 && zz>=0 &&
+														xx<width && yy<height && zz<depth)
+													if (c.getStack().getIntensity(xx,yy,zz)>newValue) {
+														newValue=c.getStack().getIntensity(xx,yy,zz);
+														xNew = xx;
+														yNew = yy;
+														zNew = zz;
+													}
+											}
+										}
+									}
+                  
+									//Check nearest neighbors
 //                             for (int a=-1; a<=1; a+=2) {
 //                                 int zz = z+a;
 //                                 if (zz>=0 && zz<depth &&
@@ -132,77 +127,72 @@ public class GradientDescent extends Processor {
 //                                     zNew=z;
 //                                 }
 //                             }
-                            x=xNew;
-                            y=yNew;
-                            z=zNew;
-                            walkTmp.add(new Pixel(x,y,z));
-                        } while (newValue>value &&
-                                flag[x][y][z]==0);
-                    }
-                    
-                    /** @todo Recursively add plateau pixels.
-                     */
-                    // Collect path data and add one visit for
-                    // the maximum
-                    if (flag[x][y][z]==0 ) { //new maximum
-                        max.add(new Pixel(x,y,z));
-                        int n=max.size();//count++?
-                        int numWalk=walkTmp.size();
-                        for (int a=0; a<numWalk; ++a) {
-                            flag[ walkTmp.elementAt(a).getX() ]
-                                    [ walkTmp.elementAt(a).getY() ]
-                                    [ walkTmp.elementAt(a).getZ() ] = n;
-                        }
-                    } else { //old maximum or background
-                        int n = flag[x][y][z];
-                        int numWalk=walkTmp.size();
-                        for (int a=0; a<numWalk; ++a) {
-                            flag[ walkTmp.elementAt(a).getX() ]
-                                    [ walkTmp.elementAt(a).getY() ]
-                                    [ walkTmp.elementAt(a).getZ() ] = n;
-                        }
-                    }
-                }
-            }
+									x=xNew;
+									y=yNew;
+									z=zNew;
+									walkTmp.add(new Pixel(x,y,z));
+								} while (newValue>value &&
+												 flag[x][y][z]==0);
+							}
+							
+							/** @todo Recursively add plateau pixels.
+							 */
+							// Collect path data and add one visit for
+							// the maximum
+							if (flag[x][y][z]==0 ) { //new maximum
+								max.add(new Pixel(x,y,z));
+								int n=max.size();//count++?
+								int numWalk=walkTmp.size();
+								for (int a=0; a<numWalk; ++a) {
+									flag[ walkTmp.elementAt(a).getX() ]
+										[ walkTmp.elementAt(a).getY() ]
+										[ walkTmp.elementAt(a).getZ() ] = n;
+								}
+							} else { //old maximum or background
+								int n = flag[x][y][z];
+								int numWalk=walkTmp.size();
+								for (int a=0; a<numWalk; ++a) {
+									flag[ walkTmp.elementAt(a).getX() ]
+										[ walkTmp.elementAt(a).getY() ]
+										[ walkTmp.elementAt(a).getZ() ] = n;
+								}
+							}
+						}
+					}
         }
-				System.out.println("Test attach0.5");
         //Save the basins of attraction
         Vector<BOA> boa = new Vector<BOA>();
         for (int i=0; i<max.size(); ++i) {
-            boa.add(new BOA(i));
+					boa.add(new BOA(i));
         }
-				System.out.println("Test attach0.5");
         for (int x=0; x<width; ++x) {
-            for (int y=0; y<height; ++y) {
-                for (int z=0; z<depth; ++z) {
-                    if (flag[x][y][z]>0) {
-                        //int tmpInt=flag[x][y][z]-1;
-                        //System.out.println(x + " " + y + " " + z + " " + tmpInt);
-                        boa.elementAt(flag[x][y][z]-1).addPixel(new Pixel(x,y,z));
-                    }
-                }
-            }
+					for (int y=0; y<height; ++y) {
+						for (int z=0; z<depth; ++z) {
+							if (flag[x][y][z]>0) {
+								//int tmpInt=flag[x][y][z]-1;
+								//System.out.println(x + " " + y + " " + z + " " + tmpInt);
+								boa.elementAt(flag[x][y][z]-1).addPixel(new Pixel(x,y,z));
+							}
+						}
+					}
         }
         // Attach the cell positions to the data in the case
         Vector<CellCenter> cc = new Vector<CellCenter>();
         int numCellCenter = max.size();
         for (int i=0; i<numCellCenter; ++i) {
-            cc.add(new CellCenter(i,max.get(i)));
+					cc.add(new CellCenter(i,max.get(i)));
         }
-				System.out.println("Test attach1");
         c.attachDataCollection(DataId.cellCenters,cc);
-				System.out.println("Test attach2");
         // Deliver the boas to the data in the case.
         c.attachDataCollection(DataId.cellBasinsOfAttraction,boa);
-				System.out.println("Test attach3");
         
         /** @todo Expand the background appropriately.
          */
         
-        System.out.println("CellcenterCounter:" + c.sizeOfData(DataId.cellCenters));
-        System.out.println("BOACounter:" + c.sizeOfData(DataId.cellBasinsOfAttraction));
-        System.out.println("Gradient Descent found " + max.size() + " cells");
+        //System.out.println("CellcenterCounter:" + c.sizeOfData(DataId.cellCenters));
+        //System.out.println("BOACounter:" + c.sizeOfData(DataId.cellBasinsOfAttraction));
+        //System.out.println("Gradient Descent found " + max.size() + " cells");
         return c;
-    }
+	}
 }
 
