77581681820a24c324d0058353603a8e4f0f47a1
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-12-07 16:10:37 +0000

    Did changes to MeanFilter. Fixes #8, fixes #7, and refs #1.

--- Costanza/CostanzaLib/src/costanza/MeanFilter.java
+++ Costanza/CostanzaLib/src/costanza/MeanFilter.java
@@ -12,78 +12,107 @@ import java.util.Vector;
  * @see Processor
  */
 public class MeanFilter extends Processor {
-
-    @Override
+	
+	@Override
     public Case process(Case c, Options options) throws Exception {
-	//System.out.println("Mean: Original Stack: " + c.getStack().getDepth());
-	Float tmpRadius = (Float) (options.getOptionValue("radius"));
-	float radius = tmpRadius.floatValue();
-	float radius2 = radius * radius;
-	int zSize = c.getStack().getDepth();
-	int ySize = c.getStack().getHeight();
-	int xSize = c.getStack().getWidth();
-
-	// Introduce a local clone
-	if (c.getStack() == null) {
-	    throw new Exception("No working stack initialised in case");
-	}
-	Stack localStack;
-	//System.out.println("Original Stack: " + c.getStack().getDepth());
-	localStack = (Stack) c.getStack().clone();
-	//System.out.println("Local Stack: " + localStack.getDepth());
-
-	// To save some multiplications
-	double zScale2 = c.getStack().getZScale() * c.getStack().getZScale();
-	double xScale2 = c.getStack().getXScale() * c.getStack().getXScale();
-	double yScale2 = c.getStack().getYScale() * c.getStack().getYScale();
-
-	// To be sure that all possible lattice points will be checked
-	int deltaIntZ = (int) (radius / c.getStack().getZScale() + 1.0f);
-	int deltaIntX = (int) (radius / c.getStack().getXScale() + 1.0f);
-	int deltaIntY = (int) (radius / c.getStack().getYScale() + 1.0f);
-
-	Vector<Integer> xList = new Vector<Integer>();
-	Vector<Integer> yList = new Vector<Integer>();
-	Vector<Integer> zList = new Vector<Integer>();
-	// Collect lattice points within the sphere
-	for (int zI = -deltaIntZ; zI <= deltaIntZ; ++zI) {
-	    for (int yI = -deltaIntY; yI <= deltaIntY; ++yI) {
-		for (int xI = -deltaIntX; xI <= deltaIntX; ++xI) {
-		    if (zI * zI * zScale2 + yI * yI * yScale2 + xI * xI * xScale2 <= radius2) {
-			zList.add(zI);
-			yList.add(yI);
-			xList.add(xI);
-		    }
+		//System.out.println("Mean: Original Stack: " + c.getStack().getDepth());
+		Float tmpRadius = (Float) (options.getOptionValue("radius"));
+		float radius = tmpRadius.floatValue();
+		float radius2 = radius * radius;
+		int zSize = c.getStack().getDepth();
+		int ySize = c.getStack().getHeight();
+		int xSize = c.getStack().getWidth();
+		
+		// Introduce a local clone
+		if (c.getStack() == null) {
+			throw new Exception("No working stack initialised in case");
 		}
-	    }
-	}
-
-	int numIndex = zList.size();
-	for (int zI = 0; zI < zSize; ++zI) {
-	    for (int yI = 0; yI < ySize; ++yI) {
-		for (int xI = 0; xI < xSize; ++xI) {
-		    float value = 0.0f;
-		    int norm = 0;
-		    for (int index = 0; index < numIndex; ++index) {
-			int zII = zI + zList.elementAt(index);
-			int yII = yI + yList.elementAt(index);
-			int xII = xI + xList.elementAt(index);
-			if (zII >= 0 && yII >= 0 && xII >= 0 &&
-				zII < zSize && yII < ySize && xII < xSize) {
-			    value += c.getStack().getIntensity(xII, yII, zII);
-			    ++norm;
+		float [][][] localStack = new float[xSize][ySize][zSize];
+		int [][][] bgFlag = new int[xSize][ySize][zSize];
+		for (int zI = 0; zI < zSize; ++zI) {
+			for (int yI = 0; yI < ySize; ++yI) {
+				for (int xI = 0; xI < xSize; ++xI) {
+					bgFlag[xI][yI][zI] = 0;
+				}
 			}
-		    }
-		    if (norm > 0) {
-			localStack.setIntensity(xI, yI, zI, value / norm);
-		    }
 		}
-	    }
+		// Get background pixels from data and set bgFlag to -1
+		StackBackground sb = (StackBackground)c.getStackData(DataId.stackBackground);
+		if (sb!=null) {
+			Vector<Pixel> bg = new Vector<Pixel>(sb);
+			int bgSize = bg.size();
+			for (int i=0; i<bgSize; ++i ) {
+				bgFlag[ bg.elementAt(i).getX() ][ bg.elementAt(i).getY() ]
+					[ bg.elementAt(i).getZ() ]=-1;
+			}
+		}
+		
+		// To save some multiplications
+		double zScale2 = c.getStack().getZScale() * c.getStack().getZScale();
+		double xScale2 = c.getStack().getXScale() * c.getStack().getXScale();
+		double yScale2 = c.getStack().getYScale() * c.getStack().getYScale();
+		
+		// To be sure that all possible lattice points will be checked
+		int deltaIntZ = (int) (radius / c.getStack().getZScale() + 1.0f);
+		int deltaIntX = (int) (radius / c.getStack().getXScale() + 1.0f);
+		int deltaIntY = (int) (radius / c.getStack().getYScale() + 1.0f);
+		
+		Vector<Integer> xList = new Vector<Integer>();
+		Vector<Integer> yList = new Vector<Integer>();
+		Vector<Integer> zList = new Vector<Integer>();
+		// Collect lattice points within the sphere
+		for (int zI = -deltaIntZ; zI <= deltaIntZ; ++zI) {
+			for (int yI = -deltaIntY; yI <= deltaIntY; ++yI) {
+				for (int xI = -deltaIntX; xI <= deltaIntX; ++xI) {
+					if (zI * zI * zScale2 + yI * yI * yScale2 + xI * xI * xScale2 <= radius2) {
+						zList.add(zI);
+						yList.add(yI);
+						xList.add(xI);
+					}
+				}
+			}
+		}
+		
+		int numIndex = zList.size();
+		for (int zI = 0; zI < zSize; ++zI) {
+			for (int yI = 0; yI < ySize; ++yI) {
+				for (int xI = 0; xI < xSize; ++xI) {
+					if (bgFlag[xI][yI][zI]==0) {
+						float value = 0.0f;
+						int norm = 0;
+						for (int index = 0; index < numIndex; ++index) {
+							int zII = zI + zList.elementAt(index);
+							int yII = yI + yList.elementAt(index);
+							int xII = xI + xList.elementAt(index);
+							if (zII >= 0 && yII >= 0 && xII >= 0 &&
+									zII < zSize && yII < ySize && xII < xSize) {
+								value += c.getStack().getIntensity(xII, yII, zII);
+								++norm;
+							}
+						}
+						if (norm > 0) {
+							localStack[xI][yI][zI] = value/norm;
+						}
+						else {
+							localStack[xI][yI][zI] = 0.0f;
+						}
+					}
+					else {
+						localStack[xI][yI][zI]=0.0f;
+					}
+				}
+			}
+		}
+		
+		//Copy the values back to the working stack
+		for (int zI = 0; zI < zSize; ++zI) {
+			for (int yI = 0; yI < ySize; ++yI) {
+				for (int xI = 0; xI < xSize; ++xI) {
+					c.getStack().setIntensity(xI,yI,zI,localStack[xI][yI][zI]);
+				}
+			}
+		}
+		
+		return c;
 	}
-
-	//Copy the values back to the working stack
-	c.setStack(localStack);
-
-	return c;
-    }
 }
