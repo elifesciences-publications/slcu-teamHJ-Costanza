018eee3b5ff16d1cd55a429b7a825d829b8c52f8
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2007-12-28 12:16:48 +0000

    Fixed deprecation warning.

--- Costanza/CostanzaTextUI/src/Viewer.java
+++ Costanza/CostanzaTextUI/src/Viewer.java
@@ -6,37 +6,37 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Viewer extends Frame {
-	private java.awt.Image image;
 
-	public Viewer(String fileName) {
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		image = toolkit.getImage(fileName);
-		MediaTracker mediaTracker = new MediaTracker(this);
-		mediaTracker.addImage(image, 0);
-		try
-		{
-			mediaTracker.waitForID(0);
-		}
-		catch (InterruptedException ie)
-		{
-			System.err.println(ie);
-			System.exit(1);
-		}
-		addWindowListener(new WindowAdapter() {
-      		public void windowClosing(WindowEvent e) {
-        		System.exit(0);
-      		}
-		});
-		setSize(image.getWidth(null), image.getHeight(null));
-		setTitle(fileName);
-		show();
-	}
+    private java.awt.Image image;
 
-	public void paint(Graphics graphics) {
-		graphics.drawImage(image, 0, 0, null);
+    public Viewer(String fileName) {
+	Toolkit toolkit = Toolkit.getDefaultToolkit();
+	image = toolkit.getImage(fileName);
+	MediaTracker mediaTracker = new MediaTracker(this);
+	mediaTracker.addImage(image, 0);
+	try {
+	    mediaTracker.waitForID(0);
+	} catch (InterruptedException ie) {
+	    System.err.println(ie);
+	    System.exit(1);
 	}
+	addWindowListener(new WindowAdapter() {
 
-	public static void main(String[] args) {
-		new Viewer(args[0]);
-	}
+	    public void windowClosing(WindowEvent e) {
+		System.exit(0);
+	    }
+	});
+	setSize(image.getWidth(null), image.getHeight(null));
+	setTitle(fileName);
+	setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+	graphics.drawImage(image, 0, 0, null);
+    }
+
+    public static void main(String[] args) {
+	new Viewer(args[0]);
+    }
 }
