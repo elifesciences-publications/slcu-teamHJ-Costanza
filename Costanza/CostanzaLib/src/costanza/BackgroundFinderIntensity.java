2686f763bb850781be9398eca637d39d16235d7a
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2008-01-22 12:57:15 +0000

    More documentation. This references #49.

--- Costanza/CostanzaLib/src/costanza/BackgroundFinderIntensity.java
+++ Costanza/CostanzaLib/src/costanza/BackgroundFinderIntensity.java
@@ -5,42 +5,50 @@ package costanza;
  * @see Processor
  */
 public class BackgroundFinderIntensity extends Processor {
-    
-	public Case process(Case c, Options options) throws Exception {
-		//System.out.println("BackgroundFinderIntensity::process");
-		
-		// Get threshold value from options
-		Float tmpThreshold = (Float) (options.getOptionValue("threshold"));
-		float threshold = tmpThreshold.floatValue();
-    
-		// Check that there is an working stack in case
-		if( c.getStack() == null) {
-			throw new Exception("No working stack available in case.");
-		}
-		
-		// Create vector with pixels that stores the background
-		StackBackground bgPixel = new StackBackground();
-		
-		int width = c.getStack().getWidth();
-		int height = c.getStack().getHeight();
-		int depth = c.getStack().getDepth();
 
-		// Extract pixels with intensity lower than threshold into background
-		for (int x=0; x<width; ++x) {
-			for (int y=0; y<height; ++y) {
-				for (int z=0; z<depth; ++z) {
-					if (c.getStack().getIntensity(x,y,z)<threshold) {
-						bgPixel.add(new Pixel(x,y,z));
-					}
-				}
-			}
-		}
-		
-		// Create background BOA and add it to data structure
-		//BOA bgBoa = new BOA(-1,bgPixel);
-		//c.getData().attachData(DataId.BACKGROUND,bgBoa);
-		c.attachStackData(bgPixel);
-                		
-		return c;
+    /**Collects all pixels below a threshold intensity and put them in the 
+     * background.
+     * @param c the Case to work on.
+     * @param options the Option object containing the "threshold" to use.
+     * @return the processed Case.
+     * @throws java.lang.Exception
+     */
+    @Override
+    public Case process(Case c, Options options) throws Exception {
+	//System.out.println("BackgroundFinderIntensity::process");
+
+	// Get threshold value from options
+	Float tmpThreshold = (Float) (options.getOptionValue("threshold"));
+	float threshold = tmpThreshold.floatValue();
+
+	// Check that there is an working stack in case
+	if (c.getStack() == null) {
+	    throw new Exception("No working stack available in case.");
 	}
+
+	// Create vector with pixels that stores the background
+	StackBackground bgPixel = new StackBackground();
+
+	int width = c.getStack().getWidth();
+	int height = c.getStack().getHeight();
+	int depth = c.getStack().getDepth();
+
+	// Extract pixels with intensity lower than threshold into background
+	for (int x = 0; x < width; ++x) {
+	    for (int y = 0; y < height; ++y) {
+		for (int z = 0; z < depth; ++z) {
+		    if (c.getStack().getIntensity(x, y, z) < threshold) {
+			bgPixel.add(new Pixel(x, y, z));
+		    }
+		}
+	    }
+	}
+
+	// Create background BOA and add it to data structure
+	//BOA bgBoa = new BOA(-1,bgPixel);
+	//c.getData().attachData(DataId.BACKGROUND,bgBoa);
+	c.attachStackData(bgPixel);
+
+	return c;
+    }
 }
