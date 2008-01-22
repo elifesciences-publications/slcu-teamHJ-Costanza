811ddf393cf7824f4a90923524ac93b5ec6583e3
Henrik Jönsson <henrik.jonsson@slcu.cam.ac.uk>
2008-01-22 14:54:08 +0000

    Made some print outs.

--- Costanza/CostanzaLib/src/costanza/Driver.java
+++ Costanza/CostanzaLib/src/costanza/Driver.java
@@ -10,12 +10,19 @@ public class Driver implements Runnable {
     /** Factory for Processor creation. */
     private Factory<Processor> factory;
     
+    /**Constructor for the Driver class.
+     * @param jobs the Queue of jobs to run.
+     * @param currentCase the Case that the jobs should work on.
+     * @param factory the Factory that we use to create each Processor.
+     */
     public Driver(Queue jobs, Case currentCase, Factory<Processor> factory) {
         this.jobs = jobs;
         this.currentCase = currentCase;
         this.factory = factory;
     }
     
+    /**Runs the jobs in the Queue. */
+    @Override
     public void run() {
 		try {
 			jobs.run(factory, currentCase);
--- Costanza/CostanzaLib/src/costanza/Queue.java
+++ Costanza/CostanzaLib/src/costanza/Queue.java
@@ -9,16 +9,21 @@ public class Queue {
         jobs = new java.util.LinkedList<Job>();
     }
     
-    /** Add job to queue. */
+    /** Add job to queue.
+     * @param job the Job to add to the Queue.
+     * @throws java.lang.Exception
+     */
     public void addJob(Job job) throws Exception {
         if (!jobs.offer(job)) {
             throw new Exception("Job queue unable to insert new job.");
         }
     }
     
-    /** Process queue.
-     *
+    /**Process queue.
      * This member function steps through the queue and calls process() in all processors.
+     * @param factory the Factory that creates each Processor.
+     * @param currentCase the Case that each Processor should work on.
+     * @throws java.lang.Exception
      */
     public void run(Factory<Processor> factory, Case currentCase) throws Exception {
         while (true) {
@@ -32,6 +37,7 @@ public class Queue {
             } catch (Exception exception) {
                 throw exception;
             }
+	    System.out.println("Running: " + job.getProcessorId());
             processor.process(currentCase, job.getOptions());
         }
     }
